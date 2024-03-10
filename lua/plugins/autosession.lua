6b019f35bddf4bb92cc2deae958c45aebd200258
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-11 00:57:32 +0300

    Add auto-session

---
+++ lua/plugins/autosession.lua
@@ -0,0 +1,23 @@
+return {
+  'rmagatti/auto-session',
+  config = function()
+    require('auto-session').setup {
+      log_level = vim.log.levels.ERROR,
+      auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      auto_session_use_git_branch = false,
+
+      auto_session_enable_last_session = true,
+
+      session_lens = {
+        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+    }
+
+    vim.keymap.set('n', '<C-s>', require('auto-session.session-lens').search_session, {
+      noremap = true,
+    })
+  end,
+}
