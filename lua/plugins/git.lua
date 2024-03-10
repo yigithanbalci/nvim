2b3403a88fe6739a68ecbee9be7065b98a21a47c
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-10 15:03:19 +0300

    Cleanup and divide plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'tpope/vim-fugitive',
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+    config = function()
+      require('gitsigns').setup()
+
+      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+    end,
+  },
+}
+
---
+++ lua/plugins/miscellaneous.lua
@@ -0,0 +1,26 @@
+return {
+    {'tpope/vim-sleuth',}, -- Detect tabstop and shiftwidth automatically
+    { 'numToStr/Comment.nvim', opts = {} }, -- "gc" to comment visual regions/lines
+    { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+}
