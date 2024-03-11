6d151528d13ae45717145a68750e668253b196f6
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-11 19:44:45 +0300

    add git fugitive basic keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -16,9 +16,13 @@ return {
     config = function()
       require('gitsigns').setup()
 
-      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
-      vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+      vim.keymap.set('n', '<leader>gsp', ':Gitsigns preview_hunk<CR>', { desc = 'Gitsigns Preview Hunk' })
+      vim.keymap.set('n', '<leader>gst', ':Gitsigns toggle_current_line_blame<CR>', { desc = 'Gitsigns Toggle Currentline Blame' })
+      vim.keymap.set('n', '<leader>gp', ':Git pull<CR>', { desc = 'Git Pull' })
+      vim.keymap.set('n', '<leader>gP', ':Git push<CR>', { desc = 'Git Pull' })
+      vim.keymap.set('n', '<leader>gA', ':Git add .<CR>', { desc = 'Git Add All' })
+      vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = 'Git Commit' })
+      vim.keymap.set('n', '<leader>gE', ':Git <CR>', { desc = 'Git Page for Manual Editing' })
     end,
   },
 }
-
