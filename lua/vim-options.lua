4bdece7bcdfc9bb13e5c3be91b59c59bc315de1b
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-12 19:11:27 +0300

    remove bg for neotree

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -115,3 +115,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_augroup('nobg', { clear = true })
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
+  desc = 'Make all backgrounds transparent',
+  group = 'nobg',
+  pattern = '*',
+  callback = function()
+    --    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'NONE', ctermbg = 'NONE' })
+    -- etc...
+  end,
+})
