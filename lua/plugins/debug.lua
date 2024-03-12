4b9e054571ca0f69a625de6df5e9f75dc0feeaee
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-13 00:10:48 +0300

    add delve back

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -39,7 +39,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        --       'delve',
+        'delve',
         'kotlin-debug-adapter',
         'java-debug-adapter',
       },
