8d2d11b9d76a9d0cfcc500490c33fbd6f1263a54
Yigithan Kadir BALCI <13695727+yigithanbalci@users.noreply.github.com>
2024-08-11 12:05:39 +0300

    add sql plugin suite (dadbotui incl)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
