e239962f3e8e9c0855d7bf500fe1d191d777bffd
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-09-25 15:26:42 +0300

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,34 +2,38 @@ return {
   --{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
   --  flavour = "macchiato",
   --} },
-  {
-    "EdenEast/nightfox.nvim",
-    name = "nightfox",
-    priority = 1000,
-    opts = function(_, opts)
-      if vim.fn.has("termguicolors") == 1 then
-        vim.opt.termguicolors = true
-      end
-      vim.cmd([[colorscheme nightfox]])
-      return opts
-    end,
-  },
   -- {
-  --   "sainnhe/gruvbox-material",
-  --   name = "gruvbox-material",
+  --   "EdenEast/nightfox.nvim",
+  --   name = "nightfox",
   --   priority = 1000,
   --   opts = function(_, opts)
   --     if vim.fn.has("termguicolors") == 1 then
   --       vim.opt.termguicolors = true
   --     end
-  --
-  --     vim.opt.background = "dark"
-  --
-  --     vim.g.gruvbox_material_background = "medium"
-  --
-  --     vim.g.gruvbox_material_better_performance = 1
-  --
-  --     vim.cmd([[colorscheme gruvbox-material]])
+  --     vim.cmd([[colorscheme nightfox]])
+  --     return opts
   --   end,
   -- },
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    priority = 1000,
+    opts = function(_, opts)
+      if vim.fn.has("termguicolors") == 1 then
+        vim.opt.termguicolors = true
+      end
+      vim.opt.background = "dark"
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "original"
+      vim.g.gruvbox_material_ui_contrast = "high"
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+
+      vim.g.gruvbox_material_better_performance = 1
+
+      vim.cmd([[colorscheme gruvbox-material]])
+    end,
+  },
 }
