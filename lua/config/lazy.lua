1e1a8753fe6345c07d5bd57012f97ed30afa97d3
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-10-05 00:10:01 +0300

    somewhat works, nvim-java not working properly

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     --{ import = "lazyvim.plugins.extras.lang.ocaml" },
-    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
