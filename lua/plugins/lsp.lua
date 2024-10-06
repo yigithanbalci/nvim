b0a131a829d3b7b591bdc893b04a32b37af63338
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-10-06 15:59:18 +0300

    rust-analyzer removed from mason, rustaceanvim uses it from os

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,21 +86,21 @@ return {
       -------------
       marksman = {},
       -- Rust Tools
-      taplo = {
-        keys = {
-          {
-            "K",
-            function()
-              if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                require("crates").show_popup()
-              else
-                vim.lsp.buf.hover()
-              end
-            end,
-            desc = "Show Crate Documentation",
-          },
-        },
-      },
+      -- taplo = {
+      --   keys = {
+      --     {
+      --       "K",
+      --       function()
+      --         if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+      --           require("crates").show_popup()
+      --         else
+      --           vim.lsp.buf.hover()
+      --         end
+      --       end,
+      --       desc = "Show Crate Documentation",
+      --     },
+      --   },
+      -- },
       -------------
       --Lua Tools
       lua_ls = {
