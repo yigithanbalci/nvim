3276c1c232b9b9408a07658a37df698fd40e023a
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-02-18 10:31:13 +0300

    remove autosession for previous sessions and old dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,29 +1,5 @@
 return {
   {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        log_level = vim.log.levels.ERROR,
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        auto_session_use_git_branch = false,
-
-        auto_session_enabled = false,
-        auto_session_enable_last_session = false,
-
-        session_lens = {
-          buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-      })
-
-      vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
-        noremap = true,
-      })
-    end,
-  },
-  {
     "snacks.nvim",
     dependencies = {
       "rmagatti/auto-session",
@@ -53,28 +29,6 @@ return {
             section = "keys",
             gap = 1,
             padding = 1,
-            keys = {
-              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-              { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-              { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-              { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-              {
-                icon = " ",
-                key = "c",
-                desc = "Config",
-                action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-              },
-              { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-              {
-                action = 'lua require("auto-session.session-lens").search_session()',
-                desc = " Search Prev Sessions",
-                icon = " ",
-                key = "S",
-              },
-
-              { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-              { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-            },
           },
           {
             pane = 2,
@@ -143,74 +97,4 @@ return {
       },
     },
   },
-  -- {
-  --   -- NOTE: Rewrite default dashbaord (instead of alpha) to add auto-session lens as action
-  --   "nvimdev/dashboard-nvim",
-  --   event = "VimEnter",
-  --   dependencies = {
-  --     "rmagatti/auto-session",
-  --     "nvim-tree/nvim-web-devicons",
-  --   },
-  --   opts = function(_, opts)
-  --     opts.config.center = {
-  --       {
-  --         action = "lua LazyVim.pick()()",
-  --         desc = " Find File",
-  --         icon = " ",
-  --         key = "f",
-  --       },
-  --       {
-  --         action = "ene | startinsert",
-  --         desc = " New File",
-  --         icon = " ",
-  --         key = "n",
-  --       },
-  --       {
-  --         action = 'lua LazyVim.pick("oldfiles")()',
-  --         desc = " Recent Files",
-  --         icon = " ",
-  --         key = "r",
-  --       },
-  --       {
-  --         action = 'lua LazyVim.pick("live_grep")()',
-  --         desc = " Find Text",
-  --         icon = " ",
-  --         key = "g",
-  --       },
-  --       {
-  --         action = "lua LazyVim.pick.config_files()()",
-  --         desc = " Config",
-  --         icon = " ",
-  --         key = "c",
-  --       },
-  --       {
-  --         action = 'lua require("persistence").load()',
-  --         desc = " Restore Session",
-  --         icon = " ",
-  --         key = "s",
-  --       },
-  --       {
-  --         action = "LazyExtras",
-  --         desc = " Lazy Extras",
-  --         icon = " ",
-  --         key = "x",
-  --       },
-  --       {
-  --         action = "Lazy",
-  --         desc = " Lazy",
-  --         icon = "󰒲 ",
-  --         key = "l",
-  --       },
-  --       {
-  --         action = function()
-  --           vim.api.nvim_input("<cmd>qa<cr>")
-  --         end,
-  --         desc = " Quit",
-  --         icon = " ",
-  --         key = "q",
-  --       },
-  --     }
-  --     return opts
-  --   end,
-  -- },
 }
