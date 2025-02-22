a9e3cf5afd33052ad2e283381f4c853a2e8642d9
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-02-22 21:38:43 +0300

    divide and remove lsp.lua

--- lua/plugins/lang/lua.lua
+++ lua/plugins/lang/lua.lua
@@ -9,4 +9,38 @@ return {
       },
     },
   },
+  {
+    --NOTE: double tap diagnostic hover (K) to enter the opened popup
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- LSP Server Settings
+      ---@type lspconfig.options
+      servers = {
+        lua_ls = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
+          -- Use this to add any additional keymaps
+          -- for specific lsp servers
+          ---@type LazyKeysSpec[]
+          -- keys = {},
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              codeLens = {
+                enable = true,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {},
+    },
+  },
 }
--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -17,4 +17,19 @@ return {
       },
     },
   },
+  {
+    --NOTE: double tap diagnostic hover (K) to enter the opened popup
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- LSP Server Settings
+      ---@type lspconfig.options
+      servers = {
+        marksman = {},
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {},
+    },
+  },
 }
