7459c4ab5f1e8dad6578ca4f65eed00bd102a1b9
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-02-22 22:14:00 +0300

    divide and remove dap.lua

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -113,4 +113,11 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "leoluz/nvim-dap-go",
+    },
+  },
 }
--- lua/plugins/lang/miscellaneous.lua
+++ lua/plugins/lang/miscellaneous.lua
@@ -119,4 +119,68 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        keys = {
+          {
+            "<leader>du",
+            function()
+              require("dapui").toggle({})
+            end,
+            desc = "Dap UI",
+          },
+          {
+            "<leader>de",
+            function()
+              require("dapui").eval(nil, { enter = true })
+            end,
+            desc = "Eval",
+            mode = { "n", "v" },
+          },
+        },
+        opts = {},
+      },
+    },
+    keys = {
+      {
+        "<F1>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<F2>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F3>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      {
+        "<F4>",
+        function()
+          require("dap").run_to_cursor()
+        end,
+        desc = "Run to Cursor",
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+    },
+  },
 }
