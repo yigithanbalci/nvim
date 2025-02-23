2bf012b6f697a24952d63f3d2243eda6562de0a0
Yigithan Kadir BALCI <13695727+yigithanbalci@users.noreply.github.com>
2025-02-23 12:58:26 +0300

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     -- { import = "plugins" },
