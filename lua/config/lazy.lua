76edd7f2bb4086a745713f33aa9656a14bad3fbc
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-02-25 12:22:42 +0300

    add mini-surround

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,9 +22,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- fundamental plugins
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
+    -- utilities
     { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- import/override with your plugins
     -- { import = "plugins" },
     { import = "plugins/core" },
