7c7aee695594a60a4e282a282380c829e4d2109c
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-02-28 18:59:24 +0300

    disable java-nvim, nvim-jdtls is buggy nowadays I guess

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -1,54 +1,55 @@
 --TODO: update this file for java projects
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "java" },
-    },
-  },
-  {
-    "nvim-java/nvim-java",
-    config = false,
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        opts = {
-          servers = {
-            -- Your JDTLS configuration goes here
-            jdtls = {
-              -- settings = {
-              --   java = {
-              --     configuration = {
-              --       runtimes = {
-              --         {
-              --           name = "JavaSE-23",
-              --           path = "/usr/local/sdkman/candidates/java/23-tem",
-              --         },
-              --       },
-              --     },
-              --   },
-              -- },
-            },
-          },
-          setup = {
-            jdtls = function()
-              -- Your nvim-java configuration goes here
-              require("java").setup({
-                -- root_markers = {
-                --   "settings.gradle",
-                --   "settings.gradle.kts",
-                --   "pom.xml",
-                --   "build.gradle",
-                --   "mvnw",
-                --   "gradlew",
-                --   "build.gradle",
-                --   "build.gradle.kts",
-                -- },
-              })
-            end,
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = {
+  --     ensure_installed = { "java" },
+  --   },
+  -- },
+  -- {
+  --   "nvim-java/nvim-java",
+  --   dependencies = {
+  --     {
+  --       "neovim/nvim-lspconfig",
+  --       opts = {
+  --         servers = {
+  --           jdtls = {
+  --             settings = {
+  --               java = {
+  --                 configuration = {
+  --                   runtimes = {
+  --                     {
+  --                       name = "JavaSE-23",
+  --                       path = "/usr/bin/java",
+  --                     },
+  --                   },
+  --                 },
+  --               },
+  --             },
+  --           },
+  --         },
+  --         setup = {
+  --           jdtls = function(_, opts)
+  --             require("java").setup({
+  --               jdk = {
+  --                 auto_install = false,
+  --               },
+  --               root_markers = {
+  --                 "settings.gradle",
+  --                 "settings.gradle.kts",
+  --                 "pom.xml",
+  --                 "build.gradle",
+  --                 "mvnw",
+  --                 "gradlew",
+  --                 "build.gradle",
+  --                 "build.gradle.kts",
+  --               },
+  --             })
+  --             return opts
+  --           end,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
