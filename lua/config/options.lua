788df907d61a53c5af9e5b87d7fc63a9e123bce9
yigithanbalciacc <yigithan.balci@accenture.com>
2025-03-04 18:04:52 +0300

    disable some langs that I dont use daily here

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -142,7 +142,7 @@ vim.g.my_config = {
       enabled = true,
     },
     go = {
-      enabled = true,
+      enabled = false,
     },
     java = {
       enabled = false,
@@ -151,7 +151,7 @@ vim.g.my_config = {
       enabled = true,
     },
     ocaml = {
-      enabled = true,
+      enabled = false,
     },
     python = {
       enabled = true,
@@ -159,14 +159,14 @@ vim.g.my_config = {
       ruff = "ruff",
     },
     rust = {
-      enabled = true,
+      enabled = false,
       diagnostics = "rust-analyzer",
     },
     typescript = {
       enabled = true,
     },
     zig = {
-      enabled = true,
+      enabled = false,
     },
   },
   ai = {
