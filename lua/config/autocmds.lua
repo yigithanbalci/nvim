8d9d5d33d6497ed750d5b9dba9828a0517fb63e7
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-03-07 23:32:52 +0300

    add removeUnused autocmd for TypeScript

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- TypeScript - Remove unused imports on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("ts_imports", { clear = true }),
+  pattern = { "*.tsx,*.ts" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.removeUnused.ts" },
+        diagnostics = {},
+      },
+    })
+  end,
+})
