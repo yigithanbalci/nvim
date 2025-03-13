785fc07f2c1d0ee654c1c9a3f05d8ef92938e824
yigithanbalciacc <yigithan.balci@accenture.com>
2025-03-13 16:55:32 +0300

    comment-out organize imports that somehow distorts code on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,15 +12,15 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = vim.api.nvim_create_augroup("ts_imports", { clear = true }),
   pattern = { "*.tsx,*.ts" },
   callback = function()
-    vim.lsp.buf.code_action({
-      apply = true,
-      context = {
-        only = {
-          "source.removeUnusedImports",
-        },
-        diagnostics = {},
-      },
-    })
+    -- vim.lsp.buf.code_action({
+    --   apply = true,
+    --   context = {
+    --     only = {
+    --       "source.removeUnusedImports",
+    --     },
+    --     diagnostics = {},
+    --   },
+    -- })
     vim.lsp.buf.code_action({
       apply = true,
       context = {
