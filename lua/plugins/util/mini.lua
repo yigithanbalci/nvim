aa731469cf38a5f91f38d9fd317c6d48ee4e3577
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-03-17 22:26:30 +0300

    mini surround mapping

--- lua/plugins/util/mini.lua
+++ lua/plugins/util/mini.lua
@@ -21,16 +21,32 @@ return {
   },
   {
     "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
+    opts = function(_, opts)
+      --NOTE yigithanbalci 17-03-2025: below configs to make mini.surround act like vim-surround
+      opts.custom_surroundings = {
+        ["("] = { output = { left = "( ", right = " )" } },
+        ["["] = { output = { left = "[ ", right = " ]" } },
+        ["{"] = { output = { left = "{ ", right = " }" } },
+        ["<"] = { output = { left = "< ", right = " >" } },
+      }
+      opts.mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+        update_n_lines = "",
+      }
+      opts.search_method = "cover_or_next"
+
+      -- Remap adding surrounding to Visual mode selection
+      vim.api.nvim_set_keymap("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+
+      -- Make special mapping for "add surrounding for line"
+      vim.api.nvim_set_keymap("n", "yss", "ys_", { noremap = false })
+
+      return opts
+    end,
   },
 }
