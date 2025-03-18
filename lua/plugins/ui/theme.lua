3ced97abacdcf3ee6fb1ea079ae3854d7f68dd60
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-03-18 09:20:07 +0300

    trying something with themes

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -27,126 +27,140 @@ function SetTheme()
   LineNumberColors()
 end
 
-return {
-  {
-    "EdenEast/nightfox.nvim",
-    name = "nightfox",
-    opts = function(_, opts)
-      SetTheme()
-      return opts
-    end,
-  },
-  {
-    "sainnhe/gruvbox-material",
-    name = "gruvbox-material",
-    opts = function(_, opts)
-      vim.o.background = vim.g.my_config.theme.scheme.background == "light" or "dark"
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_foreground = "original"
-      vim.g.gruvbox_material_ui_contrast = "high"
-      vim.g.gruvbox_material_diagnostic_line_highlight = 1
-      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+local function get_active_colorscheme()
+  local themes = {
+    ["nightfox"] = {
+      "EdenEast/nightfox.nvim",
+      name = "nightfox",
+      opts = function(_, opts)
+        SetTheme()
+        return opts
+      end,
+    },
+    ["gruvbox-material"] = {
+      "sainnhe/gruvbox-material",
+      name = "gruvbox-material",
+      opts = function(_, opts)
+        vim.o.background = vim.g.my_config.theme.scheme.background == "light" or "dark"
+        vim.g.gruvbox_material_enable_italic = 1
+        vim.g.gruvbox_material_enable_bold = 1
+        vim.g.gruvbox_material_background = "hard"
+        vim.g.gruvbox_material_foreground = "original"
+        vim.g.gruvbox_material_ui_contrast = "high"
+        vim.g.gruvbox_material_diagnostic_line_highlight = 1
+        vim.g.gruvbox_material_diagnostic_text_highlight = 1
 
-      vim.g.gruvbox_material_better_performance = 1
+        vim.g.gruvbox_material_better_performance = 1
 
-      SetTheme()
-      return opts
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        fzf = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
+        SetTheme()
+        return opts
+      end,
+    },
+    ["catppuccin"] = {
+      "catppuccin/nvim",
+      lazy = true,
+      name = "catppuccin",
+      opts = {
+        integrations = {
+          aerial = true,
+          alpha = true,
+          cmp = true,
+          dashboard = true,
+          flash = true,
+          fzf = true,
+          grug_far = true,
+          gitsigns = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          markdown = true,
+          mini = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
           },
+          navic = { enabled = true, custom_bg = "lualine" },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          semantic_tokens = true,
+          snacks = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
         },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        snacks = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
       },
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-          end
+      specs = {
+        {
+          "akinsho/bufferline.nvim",
+          optional = true,
+          opts = function(_, opts)
+            if (vim.g.colors_name or ""):find("catppuccin") then
+              opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+            end
 
-          SetTheme()
-          return opts
-        end,
+            SetTheme()
+            return opts
+          end,
+        },
       },
     },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = function(_, opts)
-      SetTheme()
-      return opts
-    end,
-  },
-  {
-    "ayu-theme/ayu-vim",
-    lazy = true,
-    opts = function(_, opts)
-      SetTheme()
-      vim.g.ayucolor = vim.g.my_config.theme.scheme.background == "light" or "dark"
-      return opts
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-    opts = function(_, opts)
-      SetTheme()
-      vim.o.background = vim.g.my_config.theme.scheme.background == "light" or "dark"
-      return opts
-    end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    opts = function(_, opts)
-      SetTheme()
-      -- opts.theme = vim.g.my_config.theme.scheme.style or "wave"
-      return opts
-    end,
-  },
-}
+    ["tokyonight"] = {
+      "folke/tokyonight.nvim",
+      lazy = true,
+      opts = function(_, opts)
+        SetTheme()
+        opts.style = "moon"
+        return opts
+      end,
+    },
+    ["ayu"] = {
+      "ayu-theme/ayu-vim",
+      lazy = true,
+      opts = function(_, opts)
+        SetTheme()
+        vim.g.ayucolor = vim.g.my_config.theme.scheme.background == "light" or "dark"
+        return opts
+      end,
+    },
+    ["gruvbox"] = {
+      "ellisonleao/gruvbox.nvim",
+      lazy = true,
+      opts = function(_, opts)
+        SetTheme()
+        vim.o.background = vim.g.my_config.theme.scheme.background == "light" or "dark"
+        return opts
+      end,
+    },
+    ["kanagawa"] = {
+      "rebelot/kanagawa.nvim",
+      lazy = true,
+      opts = function(_, opts)
+        SetTheme()
+        -- opts.theme = vim.g.my_config.theme.scheme.style or "wave"
+        return opts
+      end,
+    },
+  }
+
+  local selected_theme = vim.g.my_config.theme.scheme.name
+  if themes[selected_theme] then
+    return {
+      themes[selected_theme],
+    }
+  end
+
+  return {} -- Return an empty table if no match is found
+end
+
+return get_active_colorscheme()
