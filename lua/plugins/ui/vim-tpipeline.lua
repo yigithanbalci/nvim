a6d91895a402f9df64d824d5f3784f9c504f520b
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-03-18 09:44:07 +0300

    comment out tpipeline bcz its buggy

--- lua/plugins/ui/vim-tpipeline.lua
+++ lua/plugins/ui/vim-tpipeline.lua
@@ -1,11 +1,12 @@
 return {
-  {
-    "vimpostor/vim-tpipeline",
-    config = function()
-      -- Tpipeline for Tmux and Vim status line to merge
-      vim.g.tpipeline_autoembed = 1
-      vim.g.tpipeline_restore = 1
-      vim.g.tpipeline_clearstl = 1
-    end,
-  },
+  --TODO yigithanbalci 18-03-2025: this not working rn fix it
+  -- {
+  --   "vimpostor/vim-tpipeline",
+  --   config = function()
+  --     -- Tpipeline for Tmux and Vim status line to merge
+  --     vim.g.tpipeline_autoembed = 1
+  --     vim.g.tpipeline_restore = 1
+  --     vim.g.tpipeline_clearstl = 1
+  --   end,
+  -- },
 }
