35712a630aaadd0e99043f7c79530cce6b357e80
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-05-27 11:42:12 +0300

    disable langs not used in work

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -137,10 +137,10 @@ vim.g.my_config = {
       enabled = true,
     },
     go = {
-      enabled = true,
+      enabled = false,
     },
     flutter = {
-      enabled = true,
+      enabled = false,
       config = "flutter-tools",
     },
     java = {
@@ -150,22 +150,22 @@ vim.g.my_config = {
       enabled = true,
     },
     ocaml = {
-      enabled = true,
+      enabled = false,
     },
     python = {
-      enabled = true,
+      enabled = false,
       lsp = "pyright",
       ruff = "ruff",
     },
     rust = {
-      enabled = true,
+      enabled = false,
       diagnostics = "rust-analyzer",
     },
     typescript = {
       enabled = true,
     },
     zig = {
-      enabled = true,
+      enabled = false,
     },
   },
   ai = {
