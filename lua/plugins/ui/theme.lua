df2171be763475bebb0f29cc450556d52a06835f
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-05-30 09:50:43 +0300

    comment out catpuccin compile for transparency

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -109,22 +109,28 @@ return {
             vim.notify("No theme to toggle transparency", vim.log.levels.ERROR)
             return
           end
-          --TODO: fix inconsistent transparency when changing themes
-          if currentTheme.name:lower():find("catppuccin") then
-            local ok, catppuccin = pcall(require, "catppuccin")
-            if ok then
-              -- Toggle transparent_background
-              catppuccin.options.transparent_background = not catppuccin.options.transparent_background
-              catppuccin.compile() -- Recompile with new settings
-              themery.setThemeByName(currentTheme.name)
-            end
-          else
-            if _G.transparent_enabled then
-              SetBGTransparent()
-            end
-            _G.transparent_enabled = not _G.transparent_enabled
-            themery.setThemeByName(currentTheme.name)
+          if _G.transparent_enabled then
+            SetBGTransparent()
           end
+          _G.transparent_enabled = not _G.transparent_enabled
+          themery.setThemeByName(currentTheme.name)
+          --TODO: fix inconsistent transparency when changing themes
+          -- idk if setting by theme and compiling like below is better?
+          -- if currentTheme.name:lower():find("catppuccin") then
+          --   local ok, catppuccin = pcall(require, "catppuccin")
+          --   if ok then
+          --     -- Toggle transparent_background
+          --     catppuccin.options.transparent_background = not catppuccin.options.transparent_background
+          --     catppuccin.compile() -- Recompile with new settings
+          --     themery.setThemeByName(currentTheme.name)
+          --   end
+          -- else
+          --   if _G.transparent_enabled then
+          --     SetBGTransparent()
+          --   end
+          --   _G.transparent_enabled = not _G.transparent_enabled
+          --   themery.setThemeByName(currentTheme.name)
+          -- end
         end,
         desc = "Toggle Transparent BG (Themery)",
       },
