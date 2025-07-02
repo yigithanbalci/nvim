f7e9f1a82d3de6680af913f52564fbd6d050a67d
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-07-02 11:46:38 +0300

    update plugins & add enableProjectDiagnostics to vtsls/tsserver

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -82,6 +82,11 @@ return {
               format = {
                 enable = true,
               },
+              tsserver = {
+                experimental = {
+                  enableProjectDiagnostics = true,
+                },
+              },
             },
           },
 
