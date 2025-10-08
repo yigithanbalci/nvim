926568b76b27d3451fd325b3b6af7cfa963ffe8c
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-10-08 13:46:13 +0300

    removed redundant configs from zig

--- lua/plugins/lang/zig.lua
+++ lua/plugins/lang/zig.lua
@@ -3,34 +3,10 @@ if not vim.g.my_config.langs.zig.enabled then
 end
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "zig" } },
-  },
-  {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "zls",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        zls = {},
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "lawrence-laz/neotest-zig",
-    },
-    opts = {
-      adapters = {
-        ["neotest-zig"] = {},
       },
     },
   },
