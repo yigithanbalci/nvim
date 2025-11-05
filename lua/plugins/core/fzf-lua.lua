48c4f1191a647bfa1a877f9867308d68de3cd79c
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-11-05 11:35:38 +0300

    comment-out fzf-lua opts

--- lua/plugins/core/fzf-lua.lua
+++ lua/plugins/core/fzf-lua.lua
@@ -5,29 +5,29 @@ elseif vim.g.my_config.search == "fzf-lua" then
     --NOTE: I am using default setting of lazyvim except a couple of keybindings
     {
       "ibhagwan/fzf-lua",
-      opts = function(_, opts)
-        opts.grep = opts.grep or {}
-
-        local rg_opts = opts.grep.rg_opts or ""
-
-        -- Ensure --line-number is included
-        if not rg_opts:match("%-%-line%-number") and not rg_opts:match("%-n") then
-          rg_opts = rg_opts .. " --line-number"
-        end
-
-        -- Ensure --column is included
-        if not rg_opts:match("%-%-column") then
-          rg_opts = rg_opts .. " --column"
-        end
-
-        -- Append --multiline if not already present
-        if not rg_opts:match("%-%-multiline") then
-          rg_opts = rg_opts .. " --multiline"
-        end
-
-        opts.grep.rg_opts = vim.trim(rg_opts)
-        return opts
-      end,
+      -- opts = function(_, opts)
+      --   opts.grep = opts.grep or {}
+      --
+      --   local rg_opts = opts.grep.rg_opts or ""
+      --
+      --   -- Ensure --line-number is included
+      --   if not rg_opts:match("%-%-line%-number") and not rg_opts:match("%-n") then
+      --     rg_opts = rg_opts .. " --line-number"
+      --   end
+      --
+      --   -- Ensure --column is included
+      --   if not rg_opts:match("%-%-column") then
+      --     rg_opts = rg_opts .. " --column"
+      --   end
+      --
+      --   -- Append --multiline if not already present
+      --   if not rg_opts:match("%-%-multiline") then
+      --     rg_opts = rg_opts .. " --multiline"
+      --   end
+      --
+      --   opts.grep.rg_opts = vim.trim(rg_opts)
+      --   return opts
+      -- end,
       keys = {
         { "<leader><space>", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
       },
