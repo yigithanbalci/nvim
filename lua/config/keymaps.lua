f2339560cfa0006e3b43d26e4c01a0f845c2a436
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-26 10:06:51 +0300

    add lazyvim default git keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -67,3 +67,20 @@ map("n", "<leader>fs", "<cmd>noautocmd w<cr>", { desc = "Save without formatting
 -- there is a problem while saving it inserts inputs
 -- probably because formatting is too slow, not this mapping
 -- map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- LazyVim v15.14.0 Defaults for Git  --
+--#region
+-- LazyGit
+-- if vim.fn.executable("lazygit") == 1 then
+--   map("n", "<leader>gg", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
+--   map("n", "<leader>gG", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
+-- end
+-- map("n", "<leader>gL", function() Snacks.picker.git_log() end, { desc = "Git Log (cwd)" })
+-- map("n", "<leader>gb", function() Snacks.picker.git_log_line() end, { desc = "Git Blame Line" })
+-- map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
+-- map("n", "<leader>gl", function() Snacks.picker.git_log({ cwd = LazyVim.root.git() }) end, { desc = "Git Log" })
+-- map({ "n", "x" }, "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse (open)" })
+-- map({"n", "x" }, "<leader>gY", function()
+--   Snacks.gitbrowse({ open = function(url) vim.fn.setreg("+", url) end, notify = false })
+-- end, { desc = "Git Browse (copy)" })
+--#endregion
