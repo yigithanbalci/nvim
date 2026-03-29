b515fadeb6239570b23888c985b0d26055e1ebd2
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-29 13:58:10 +0300

    add todo and remove comments fzf-lua

--- lua/plugins/editor/fzf-lua.lua
+++ lua/plugins/editor/fzf-lua.lua
@@ -2,32 +2,9 @@ if _G.yeet.plugins.search == "telescope" then
   return {}
 elseif _G.yeet.plugins.search == "fzf-lua" then
   return {
-    --NOTE: I am using default setting of lazyvim except a couple of keybindings
+    -- TODO yigithanbalci 2026-03-29: Add multiline match for grep
     {
       "ibhagwan/fzf-lua",
-      -- opts = function(_, opts)
-      --   opts.grep = opts.grep or {}
-      --
-      --   local rg_opts = opts.grep.rg_opts or ""
-      --
-      --   -- Ensure --line-number is included
-      --   if not rg_opts:match("%-%-line%-number") and not rg_opts:match("%-n") then
-      --     rg_opts = rg_opts .. " --line-number"
-      --   end
-      --
-      --   -- Ensure --column is included
-      --   if not rg_opts:match("%-%-column") then
-      --     rg_opts = rg_opts .. " --column"
-      --   end
-      --
-      --   -- Append --multiline if not already present
-      --   if not rg_opts:match("%-%-multiline") then
-      --     rg_opts = rg_opts .. " --multiline"
-      --   end
-      --
-      --   opts.grep.rg_opts = vim.trim(rg_opts)
-      --   return opts
-      -- end,
       keys = {
         { "<leader><space>", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
         { "<leader>gd", false }, -- Disable LazyVim's fzf-lua Git Diff so diffview.nvim owns <leader>gd
